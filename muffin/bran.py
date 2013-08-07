433a2a0c92e21db4e1d3f4fa1ea4f21341d7be9a
Corbin Simpson <MostAwesomeDude@gmail.com>
2013-08-06 22:45:09 -0700

    Break ground on regex implementation.

---
+++ muffin/bran.py
@@ -0,0 +1,8 @@
+from muffin.core import Alt, Cat, Exactly, Rep
+
+
+char = Alt(Exactly("a"), Exactly("b"))
+
+charset = Cat(Exactly("["), Cat(Rep(char), Exactly("]")))
+
+expr = Rep(Alt(char, charset))
