50719aec1a4603711693928a2b3ddd05bcf8c0f5
Corbin Simpson <MostAwesomeDude@gmail.com>
2013-08-30 08:50:03 -0700

    berry/json: Optimize and shrink.
    Turns out that more powerful primitives and combinators enable more powerful
    parsers. Magic!

--- muffin/berry/json.py
+++ muffin/berry/json.py
@@ -1,7 +1,16 @@
 import string
 
-from muffin.cups import All, Any, String
-from muffin.pan import Alt, Cat, Ex, Set, rec, tie
+from muffin.cups import All, Any, Sep, String
+from muffin.pan import Alt, Cat, Ex, Red, Rep, Set, rec, tie
+
+
+def OneOrMore(l):
+    """
+    Match one or more of the given language.
+    """
+
+    return Red(Cat(l, Rep(l)),
+               lambda (car, cdr): (car,) + cdr if cdr else (car,))
 
 
 # Primitives.
@@ -11,7 +20,7 @@ digit19 = Set("123456789")
 e = Any([Ex("e"), Ex("E"), String("e-"), String("e+"), String("E-"),
          String("E+")])
 
-digits = Alt(digit, Cat(digit, rec("digits")))
+digits = OneOrMore(digit)
 
 exp = Cat(e, digits)
 
@@ -25,23 +34,22 @@ number = Any([int, Cat(int, frac), Cat(int, exp), All([int, frac, exp])])
 #XXX
 char = Any([Set(string.letters)])
 
-chars = Alt(char, Cat(char, rec("chars")))
+chars = OneOrMore(char)
 
 string = Alt(String('""'), All([Ex('"'), chars, Ex('"')]))
 
 value = Any([string, number, rec("obj"), rec("array"), String("true"),
              String("false"), String("null")])
 
-elements = Alt(value, All([value, Ex(","), rec("elements")]))
+elements = Sep(value, Ex(","))
 
 array = Alt(String("[]"), All([Ex("["), elements, Ex("]")]))
 
 pair = All([string, Ex(":"), value])
 
-members = Alt(pair, All([pair, Ex(","), rec("members")]))
+members = Sep(pair, Ex(","))
 
 obj = Alt(String("{}"), All([Ex("{"), members, Ex("}")]))
 
 
-tie(value, {"array": array, "chars": chars, "digits": digits,
-            "elements": elements, "members": members, "obj": obj})
+tie(value, {"array": array, "obj": obj})
